f5d95ab3df0cbae52ede0696b55005d017b42f98
saulfl54 <saulfl54@educastur.es>
2022-10-28 23:05:38 +0200

    Comienzo del Proyecto

---
+++ src/main/java/com/connectworkers/springboot/web/app/ConnectWorkersApplication.java
@@ -0,0 +1,13 @@
+package com.connectworkers.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectWorkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectWorkersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connectworkers/springboot/web/app/ConnectWorkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectworkers.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectWorkersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
